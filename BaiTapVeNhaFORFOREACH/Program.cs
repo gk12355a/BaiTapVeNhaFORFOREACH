212a3ea297f7f3be78ea9fca572b676a25677ec8
gk12355a <kiendotri@gmail.com>
2024-06-18 06:56:53 +0700

    Add project files.

---
+++ BaiTapVeNhaFORFOREACH/Program.cs
@@ -0,0 +1,19 @@
+﻿//Bai 3.1
+using System;
+
+namespace MultiplesOfThree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 300; i >= 3; i--)
+            {
+                if (i % 3 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
